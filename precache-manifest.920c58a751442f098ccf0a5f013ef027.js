5778e0f8a5d6b7f60675dcd7fba4179615056bd9
Nikola Bojanovic <nikolabojanovicmob@gmail.com>
2019-03-10 13:33:13 +0100

    Updates

---
+++ precache-manifest.920c58a751442f098ccf0a5f013ef027.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "947475937af92ad28604b7b2cac54ff3",
+    "url": "/mama-mia/static/media/bg.94747593.jpg"
+  },
+  {
+    "revision": "14646a5f40a6b318df55",
+    "url": "/mama-mia/static/js/runtime~main.14646a5f.js"
+  },
+  {
+    "revision": "bc2d0ab1d00a06677a29",
+    "url": "/mama-mia/static/js/main.bc2d0ab1.chunk.js"
+  },
+  {
+    "revision": "28afb4d0300b1a804e64",
+    "url": "/mama-mia/static/js/2.28afb4d0.chunk.js"
+  },
+  {
+    "revision": "bc2d0ab1d00a06677a29",
+    "url": "/mama-mia/static/css/main.1a8b2aa0.chunk.css"
+  },
+  {
+    "revision": "44917c7588610da74a3a5543b0323937",
+    "url": "/mama-mia/index.html"
+  }
+];
